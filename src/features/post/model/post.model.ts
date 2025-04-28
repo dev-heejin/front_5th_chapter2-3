46d53e6c5983295be1305d4019152d1939224741
Heejin <dev.heejiin@gmail.com>
2025-04-28 20:46:06 +0900

    refactor: comment, post, user 모델 분리

---
+++ src/features/post/model/post.model.ts
@@ -0,0 +1,18 @@
+import { User } from "../../user/model/user.model.ts"
+
+export interface Post {
+  id: number
+  userId: number
+  title: string
+  body: string
+  tags: string[]
+  reactions: {
+    likes: string
+    dislikes: string
+  }
+  views: number
+}
+
+export interface PostWithUser extends Post {
+  author?: User
+}
